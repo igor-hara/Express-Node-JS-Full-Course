8569899e2763318393f795ae58f3e1a465b26976
igor-hara <munkieryder@gmail.com>
2024-10-01 12:39:37 +0200

    add/save session id for user

--- src/index.js
+++ src/index.js
@@ -5,18 +5,33 @@ import morgan from 'morgan';
 // import productsRouter from './routes/products.js';
 import routes from './routes/index.js';
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser());
 app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(session({
+    secret: `${process.env.SESSION_SECRET}`,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 60000 * 60
+    }
+  }
+));
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
+
+  console.log(req.session);
+  console.log(req.session.id);
+  // tracks user with this session id
+  req.session.visited = true;
   res.cookie('my-cookie', 'test', { maxAge: 60000 * 60 });
   res.send('Hello World!');
 });
