d80460fd4e9b70b16f244f6c0acdf5ffae6048da
igor-hara <munkieryder@gmail.com>
2024-10-03 11:41:19 +0200

    add/passport-local, auth user with passport

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import morgan from 'morgan';
 import routes from './routes/index.js';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
+import passport from 'passport';
+import '../src/strategies/local-strategy.js';
 
 import { users } from './utils/helpers.js';
 
@@ -24,9 +26,23 @@ app.use(session({
     }
   }
 ));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
+
+app.post('/api/auth', passport.authenticate('local'), (req, res) => {
+  res.sendStatus(200);
+});
+
+app.get('/api/auth/status', (req, res) => {
+  console.log('req user ', req.user);
+  // see object how user and passport are connected (stores and connects user id into passport)
+  console.log('session user ', req.session);
+  return req.user ? res.send(req.user) : res.sendStatus(401);
+});
 
 app.get('/', (req, res) => {
 
@@ -39,31 +55,31 @@ app.get('/', (req, res) => {
 });
 
 // Session part 2. session exercise, example for Auth
-app.post('/api/auth', (req, res) => {
-  const { name, password } = req.body;
-
-  // TODO: validate username and password
-
-  const findUser = users.find(user => user.name === name && user.password === password);
-
-  // console.log('findUser', findUser);
-  if (!findUser) {
-    return res.sendStatus(401).send({ msg: 'Invalid username or password' });
-  }
-  req.session.user = findUser;
-  return res.status(200).send(findUser);
-});
+// app.post('/api/auth', (req, res) => {
+//   const { name, password } = req.body;
+//
+//   // TODO: validate username and password
+//
+//   const findUser = users.find(user => user.name === name && user.password === password);
+//
+//   // console.log('findUser', findUser);
+//   if (!findUser) {
+//     return res.sendStatus(401).send({ msg: 'Invalid username or password' });
+//   }
+//   req.session.user = findUser;
+//   return res.status(200).send(findUser);
+// });
 
 // check if user is logged in
-app.get('/api/auth/status', (req, res) => {
-
-  // show i.e. map users from sessionStore
-  req.sessionStore.get(req.sessionID, (err, session) => {
-    console.log('user-session: ', session);
-  });
-
-  return req.session.user ? res.status(200).send(req.session.user) : res.status(401).send({ msg: 'Not Authenticated' });
-});
+// app.get('/api/auth/status', (req, res) => {
+//
+//   // show i.e. map users from sessionStore
+//   req.sessionStore.get(req.sessionID, (err, session) => {
+//     console.log('user-session: ', session);
+//   });
+//
+//   return req.session.user ? res.status(200).send(req.session.user) : res.status(401).send({ msg: 'Not Authenticated' });
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
